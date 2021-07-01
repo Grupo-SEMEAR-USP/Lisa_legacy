0b0b50ca7628c93984b39817db929de6c01900d5
Rodrigo-P <rodrigobragatopiva@gmail.com>
2021-07-01 19:20:06 -0300

    Merge

--- Esqueleto/comandos.py
+++ Esqueleto/comandos.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-from enum import Enum
-
-class comando():
-	acordar = '0'
-	ofensa = '1'
-	desculpa = '2'
-	elogio = '3'
-	silencio = '4'
-	dance = '5'
-	stop = '6'
-=======
 class comando:
 	
 	ofensa = 0
@@ -27,7 +15,6 @@ class comando:
 	ADA = 12
 	Musica = 13
 	Soletrar = 14
->>>>>>> 62528980d0e3b76836d97623ee8273448dd28a74
 
 	def _init_(self):
 		pass
