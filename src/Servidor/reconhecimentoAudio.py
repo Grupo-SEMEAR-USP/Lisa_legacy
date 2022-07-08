c22f4d38be9eec6ce2295103259f7488524e6d21
gianvstheworld <gianlucacapezzuto@usp.br>
2022-07-08 15:06:12 -0300

    Verificar problemas com a janela computacional

--- src/Servidor/reconhecimentoAudio.py
+++ src/Servidor/reconhecimentoAudio.py
@@ -2,49 +2,55 @@ import speech_recognition as sr
 import numpy as np
 from scipy.io import wavfile
 from python_speech_features import mfcc, logfbank
+from io import BytesIO
 
-# Função de tratamento de áudio, basicamente implementa um filtro passa-faixa
 def trataAudio(audio):
+    ''' 
+    A função trataAudio retorna o áudio de entrada com uma 
+    filtragem específica para voz humana,
+    resumindo é feito um filtro passa-faixa 
+    '''
+
     # Define a frequência da amostra e lê o sinal do arquivo de áudio
-    f_sample, signal_audio = (wavfile.read(audio)) 
+    freq_amostra, sinal_audio = (wavfile.read(BytesIO(audio))) 
 
     # Normalizando o sinal
-    pow_audio = signal_audio / np.power(2, 15)
+    pow_audio = sinal_audio / np.power(2, 15)
     pow_audio = pow_audio[:100]
-    time = 1000 * np.arrange(0, len(pow_audio), 1) / float(f_sample)
 
-    signal_audio = signal_audio / np.power(2, 15)
+    sinal_audio = sinal_audio / np.power(2, 15)
 
     # Agora, trabalhando no mesmo de arquivo de entrada para aplicar a Transformada de Fourier
-    signal_len = len(signal_audio)
-    half_len = np.ceil((signal_len + 1) / 2.0).astype(np.int)
+    sinal_tam = len(sinal_audio)
+    metade_tam = np.ceil((sinal_tam + 1) / 2.0).astype(int)
 
     # Usando a Fast Fourier transform para formar o domínio da frequência do sinal
-    sinal_f = np.fft.fft(signal_audio)
+    sinal_f = np.fft.fft(sinal_audio)
 
     # Normalizando o domínio da frequência
-    sinal_f = abs(sinal_f[0:half_len]) / signal_len
+    sinal_f = abs(sinal_f[0:metade_tam]) / sinal_tam
     sinal_f **= 2
-    transform_len = len(sinal_f)
+    transformada_tam = len(sinal_f)
 
     # Ajustando a transformada para casos pares e ímpares 
-    if signal_len % 2:
-        sinal_f[1:transform_len] *= 2
+    if sinal_tam % 2:
+        sinal_f[1:transformada_tam] *= 2
     else:
-        sinal_f[1:transform_len - 1] *= 2
+        sinal_f[1:transformada_tam - 1] *= 2
 
     # Agora, serão pegos as primeiras 15000 amostras do sinal de aúdio
-    signal_audio = signal_audio[:15000]
+    sinal_audio = sinal_audio[:15000]
 
     # Usando o Coeficiente de Frequência Cepstral-Mel para extrair os recuros do sinal (extrai apenas a voz humana)
-    mfcc_recursos = mfcc(signal_audio, f_sample)
+    mfcc_recursos = mfcc(sinal_audio, freq_amostra)
     mfcc_recursos = mfcc_recursos.T
 
     #Gerando os recursos do banco de filtro
-    fb_recursos = logfbank(signal_audio, f_sample)
+    fb_recursos = logfbank(sinal_audio, freq_amostra)
     fb_recursos = fb_recursos.T
 
-    return mfcc_recursos #TODO verificar se, de fato, são as features do MFCC que são retornadas 
+    # Retorna a distribuição do banco de filtos no qual fica evidente a disposição das baixas e altas frequências
+    return fb_recursos 
 
 def reconhecerAudio(audio, sample_rate=44100, sample_width=2):
     rec = sr.Recognizer()
@@ -71,6 +77,7 @@ if __name__ == "__main__":
     arq = open("exemplo.wav", "rb")
     audio = arq.read()
 
+    # audio_tratado = trataAudio(audio)
     texto = reconhecerAudio(audio)
     print(texto)
 
