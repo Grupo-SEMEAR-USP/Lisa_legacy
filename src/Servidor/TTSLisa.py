c0e97e7666460fa89bd5d63769d003188256a033
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2022-07-17 15:14:31 -0300

    Melhora no TTS inicial implementada

--- src/Servidor/TTSLisa.py
+++ src/Servidor/TTSLisa.py
@@ -1,26 +1,44 @@
-from gtts import gTTS
+import gtts
+import pydub
+import pyrubberband as pyrb
+import numpy as np
+import io
 
 
-#TODO:  o audio sai como .mp3, temos que converter esse audio para wav para a Lisa
-#       melhorar o TTS com librosa
+def gerarAudio(texto):
+    '''
+    A função gerarAudio cria um áudio de Text to Speech a partir de um texto,
+    sendo baseado no text to speech do google, mas com uma mudança de voz para
+    ser mais amigável e menos cansativo de ouvir. O áudio de retorno é um array
+    de numpy contendo todo o áudio descomprimido.
+    '''
+    tts = gtts.gTTS(texto, lang="pt")
 
+    dados_mp3 = io.BytesIO()
+    tts.write_to_fp(dados_mp3)
+    dados_mp3.seek(0)
 
-def gerarStreamAudio(texto):
-    tts = gTTS(texto, lang="pt")
+    audio = pydub.AudioSegment.from_mp3(dados_mp3)
+    audio.set_channels(1)
 
-    return tts.stream
+    dados_wav = np.array(audio.get_array_of_samples())
+
+    dados_melhor = pyrb.pitch_shift(
+                dados_wav, 24000, 1.5, rbargs={"-F":"-F", "-t":0.65, "-c":6}
+    )
+
+    return dados_melhor
 
 
 if __name__ == "__main__":
+    import scipy.io.wavfile
     import playsound
+    import time
     import os
 
-    gerador = gerarStreamAudio("Oi! Eu sou a Lisa")
+    dados = gerarAudio("Oi! Eu sou a Lisa")
     
-    arq = open("exemplo.mp3", "wb")
-    for s in gerador():
-        arq.write(s)
-    arq.close()
+    scipy.io.wavfile.write("exemplo.wav", 24000, dados)
 
-    playsound.playsound("exemplo.mp3")
-    os.remove("exemplo.mp3")
+    playsound.playsound("exemplo.wav")
+    os.remove("exemplo.wav")
