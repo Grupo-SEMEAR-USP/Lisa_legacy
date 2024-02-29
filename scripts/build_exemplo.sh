e35ae3c1dbe0ed02698cddd2fa3dc66f17433f33
Thallys Oliveira <thallys.smo@gmail.com>
2024-02-29 15:37:38 -0300

    Generalizando

--- scripts/build_exemplo.sh
+++ scripts/build_exemplo.sh
@@ -9,5 +9,5 @@
 # If you want to build all the packages in release with debug info [RECOMMENDED]
 catkin config -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
-# Build all the workspace
+# Build all the workspace -> Os ws abaixos serão criados, eles vem do setup, ou seja, os arquivos clonados serão constridos nessas pastas
 catkin build ocs2_legged_robot_ros legged_controllers legged_unitree_description legged_gazebo
