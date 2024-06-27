625bc088c18832f9c1a5d3d5230fba849d109406
Matheus Hipólito Carvalho <102363489+MHC-CodeSmith@users.noreply.github.com>
2024-06-27 11:27:43 -0300

    Create kinect.py

---
+++ scripts/kinect.py
@@ -0,0 +1,35 @@
+import cv2
+import mediapipe as mp
+
+# Initialize MediaPipe Pose.
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils
+
+# Initialize video capture from webcam.
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        print("Ignoring empty camera frame.")
+        continue
+
+    # To improve performance, optionally mark the image as not writeable to pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = pose.process(image)
+
+    # Draw the pose annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    cv2.namedWindow("Video", cv2.WINDOW_NORMAL )
+    cv2.imshow('Video', image)
+    if cv2.waitKey(5) & 0xFF == 27:  # Press 'ESC' to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
